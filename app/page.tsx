b9ee0e048ceb20cd24bb6bd7894ffb065eda6b8d
pehlivanu <pehlivanu@gmail.com>
2025-01-04 16:14:34 +0100

    Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-white">
+    <h1 className="h1-xl font-black text-white">
       Welcome to the world of Next.js
     </h1>
   );
