9315352ffd3c133feb729c9062dc42d1f39b8b33
pehlivanu <pehlivanu@gmail.com>
2025-01-07 21:00:25 +0100

    Updated the home page layout

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,8 +2,14 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id: string) => `/profile/${id}`,
   TAGS: (id: string) => `/tags/${id}`,
+  JOB: (id: string) => `/jobs/${id}`,
+  JOBS: "/jobs",
+  COLLECTION: (id: string) => `/collections/${id}`,
+  COLLECTIONS: "/collections",
+  COMMUNITY: (id: string) => `/community/${id}`,
 };
 
 export default ROUTES;
