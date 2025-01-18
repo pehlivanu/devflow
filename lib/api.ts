a1ff65c7005582d151371ad2c752f715f3fe08b1
pehlivanu <pehlivanu@gmail.com>
2025-01-18 16:46:13 +0100

    Implement OAuth

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,4 @@
+import ROUTES from "@/constants/routes";
 import { IAccount } from "@/database/account.model";
 import { IUser } from "@/database/user.model";
 
@@ -7,6 +8,18 @@ const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
 export const api = {
+  auth: {
+    oAuthSignIn: ({
+      user,
+      provider,
+      providerAccountId,
+    }: SignInWithOAuthParams) =>
+      fetchHandler(`${API_BASE_URL}/auth/${ROUTES.SIGN_IN_WITH_OAUTH}`, {
+        method: "POST",
+        body: JSON.stringify({ user, provider, providerAccountId }),
+      }),
+  },
+
   users: {
     getAll: () => fetchHandler(`${API_BASE_URL}/users`),
     getById: (id: string) => fetchHandler(`${API_BASE_URL}/users/${id}`),
@@ -28,6 +41,7 @@ export const api = {
     delete: (id: string) =>
       fetchHandler(`${API_BASE_URL}/users/${id}`, { method: "DELETE" }),
   },
+
   accounts: {
     getAll: () => fetchHandler(`${API_BASE_URL}/accounts`),
     getById: (id: string) => fetchHandler(`${API_BASE_URL}/accounts/${id}`),
